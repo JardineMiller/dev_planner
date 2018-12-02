84bc3113bb7cb95614db8ce237fa62ebb965915a
Jardine <jardine.miller@gmail.com>
2018-12-02 13:51:45 +0000

    Started use of the repo pattern

--- dev_planner_backend/dev_planner_backend/Controllers/ItemsController.cs
+++ dev_planner_backend/dev_planner_backend/Controllers/ItemsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using dev_planner_backend.Contexts;
 using dev_planner_backend.Services;
 using dev_planner_backend.Services.Mail;
+using dev_planner_backend.Services.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,23 +17,42 @@ namespace dev_planner_backend.Controllers
     [Route("api/items")]
     public class ItemsController : Controller
     {
-        private ApplicationDbContext cnt;
+        private IItemRepository repo;
         private readonly ILogger<ItemsController> logger;
         private readonly IMailService mailService;
 
-        public ItemsController(ApplicationDbContext cnt, ILogger<ItemsController> logger, IMailService mailService)
+        public ItemsController(IItemRepository repo, ILogger<ItemsController> logger, IMailService mailService)
         {
-            this.cnt = cnt;
+            this.repo = repo;
             this.logger = logger;
             this.mailService = mailService;
         }
 
         [HttpGet]
-        public IActionResult TestDatabase()
+        public IActionResult GetItems()
         {
-            mailService.Send("Test Email", "This is another test email to indicate that stuff is actually somehow working.");
-            return Ok();
+            return Ok(repo.GetItems());
+        }
+
+        [HttpGet("{itemId}")]
+        public IActionResult GetItem(int itemId)
+        {
+            if (!repo.ItemExists(itemId)) return NotFound();
+            return Ok(repo.GetItem(itemId, getFull: true));
+        }
+
+        [HttpGet("{itemId}/state")]
+        public IActionResult GetItemState(int itemId)
+        {
+            if (!repo.ItemExists(itemId)) return NotFound();
+            return Ok(repo.GetItemState(itemId));
         }
         
+        [HttpGet("{itemId}/owner")]
+        public IActionResult GetItemOwner(int itemId)
+        {
+            if (!repo.ItemExists(itemId)) return NotFound();
+            return Ok(repo.GetItemOwner(itemId));
+        }
     }
 }
