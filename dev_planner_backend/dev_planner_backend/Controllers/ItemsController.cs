202e38eea5650a51f739e2cab579d54fef261664
Jardine <jardine.miller@gmail.com>
2018-12-02 21:21:23 +0000

    Added ItemController methods to return the full entity with all includes

--- dev_planner_backend/dev_planner_backend/Controllers/ItemsController.cs
+++ dev_planner_backend/dev_planner_backend/Controllers/ItemsController.cs
@@ -34,19 +34,43 @@ namespace dev_planner_backend.Controllers
             return Ok(repo.Get());
         }
 
+        [HttpGet("full")]
+        public IActionResult GetFullItems()
+        {
+            var items = repo.Query()
+                .Include(i => i.State)
+                .Include(i => i.Owner);
+
+            return Ok(items);
+        }
+
         [HttpGet("{itemId}")]
         public IActionResult GetItem(int itemId)
         {
-            var item = repo.Query(i => i.Id == itemId)
-                .Include(i => i.State)
-                .Include(i => i.Owner)
-                .FirstOrDefault();
-            
+            var item = repo.Get(i => i.Id == itemId).FirstOrDefault();
             if (item != null)
             {
                 return Ok(item);
             }
 
+            logger.LogWarning($"Item with ID: {itemId} not found in the database.");
+            return NotFound();
+        }
+
+        [HttpGet("{itemId}/full")]
+        public IActionResult GetFullItem(int itemId)
+        {
+            var item = repo.Query(i => i.Id == itemId)
+                .Include(i => i.State)
+                .Include(i => i.Owner)
+                .FirstOrDefault();
+
+            if (item != null)
+            {
+                return Ok(item);
+            }
+
+            logger.LogWarning($"Item with ID: {itemId} not found in the database.");
             return NotFound();
         }
 
@@ -56,28 +80,77 @@ namespace dev_planner_backend.Controllers
             var item = repo.Query(i => i.Id == itemId)
                 .Include(i => i.State)
                 .FirstOrDefault();
-            
+
             if (item != null)
             {
                 return Ok(item.State);
             }
 
+            logger.LogWarning($"Item with ID: {itemId} not found in the database.");
             return NotFound();
         }
-        
+
         [HttpGet("{itemId}/owner")]
         public IActionResult GetItemOwner(int itemId)
         {
             var item = repo.Query(i => i.Id == itemId)
                 .Include(i => i.Owner)
                 .FirstOrDefault();
-            
+
             if (item != null)
             {
                 return Ok(item.Owner);
             }
 
+            logger.LogWarning($"Item with ID: {itemId} not found in the database.");
             return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult InsertItem([FromBody] Item item)
+        {
+            try
+            {
+                var savedItem = repo.Insert(item);
+                return Ok(savedItem.Id);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return StatusCode(500, $"Something went wrong and your create request could not be processed. Please try again.");
+            }
+        }
+
+        [HttpPut("{itemId}")]
+        public IActionResult UpdateItem(int itemId, [FromBody] Item item)
+        {
+            if (itemId != item.Id) return BadRequest("The ID in the URL does not match the ID of the object provided.");
+            
+            try
+            {
+                repo.Update(item);
+                return Ok(item);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return StatusCode(500, $"Something went wrong and your create request could not be processed. Please try again.");
+            }
+        }
+
+        [HttpDelete("{itemId}")]
+        public IActionResult DeleteItem(int itemId)
+        {
+            try
+            {
+                repo.Delete(itemId);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return StatusCode(500, $"Something went wrong and your create request could not be processed. Please try again.");
+            }
         }
     }
 }
