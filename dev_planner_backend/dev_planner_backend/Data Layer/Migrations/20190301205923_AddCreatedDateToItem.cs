6f701b15b3fa3c050b26ff5af03d53de26f17fd9
Jardine <jardine.miller@gmail.com>
2019-03-01 21:20:18 +0000

    Migrations for adding a CreatedDate field to Item. For some reason it wasn't included in the previous commit

---
+++ dev_planner_backend/dev_planner_backend/Data Layer/Migrations/20190301205923_AddCreatedDateToItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dev_planner_backend.Migrations
+{
+    public partial class AddCreatedDateToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Items",
+                nullable: false,
+                defaultValue: new DateTimeOffset(1970, 1, 1, 0, 0, 0,  TimeSpan.Zero));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Items");
+        }
+    }
+}
